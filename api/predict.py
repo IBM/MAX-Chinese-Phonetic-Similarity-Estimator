a5a4f4c3f48c1ef2e303b9bc6a44cf47872c02f8
Brendan Dwyer <bmdsherman@gmail.com>
2019-06-05 15:21:21 -0700

    add flake8 (#9)
    * add flake8
    
    * install flake8
    
    * Create requirements-test.txt
    
    * pip install from requirements-test.txt

--- api/predict.py
+++ api/predict.py
@@ -1,7 +1,6 @@
 from core.model import ModelWrapper
 from maxfw.core import MAX_API, PredictAPI
 from flask_restplus import fields
-from werkzeug.datastructures import FileStorage
 from flask import abort
 
 # Set up parser for input data (http://flask-restplus.readthedocs.io/en/stable/parsing.html)
@@ -10,10 +9,11 @@ input_parser.add_argument('first_word', type=str, required=True,
                           help="utf-8 encoded Chinese word.")
 input_parser.add_argument('second_word', type=str,
                           help="utf-8 encoded Chinese word.")
-input_parser.add_argument('mode', type=str, default ='simplified', choices=['simplified', 'traditional'],
+input_parser.add_argument('mode', type=str, default='simplified', choices=['simplified', 'traditional'],
                           help="Chinese: simplified or traditional.")
-input_parser.add_argument('theta', type=int, default=1, 
-                          help="Distance threshold for number of candidate words to return. A higher theta returns more candidate words")
+input_parser.add_argument('theta', type=int, default=1,
+                          help="Distance threshold for number of candidate words to return. A higher theta returns more"
+                               "candidate words")
 
 
 # Creating a JSON response model: https://flask-restplus.readthedocs.io/en/stable/marshalling.html#the-api-model-factory
