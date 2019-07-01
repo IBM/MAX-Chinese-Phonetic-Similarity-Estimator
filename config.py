ba454c41a09ec54f2bc072898b7e9521331b34ec
Karthik Muthuraman <karthik.muthuraman@ibm.com>
2019-07-01 14:47:44 -0700

    Add license headers.

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
+#
+# Copyright 2018-2019 IBM Corp. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Flask settings
 DEBUG = False
 
@@ -8,7 +23,7 @@ SWAGGER_UI_DOC_EXPANSION = 'none'
 # API metadata
 API_TITLE = 'MAX Chinese Phonetic Similarity Estimator'
 API_DESC = 'Estimate the phonetic distance between Chinese words and get similar sounding candidate words.'
-API_VERSION = '1.1.0'
+API_VERSION = '1.2.0'
 
 # default model
 MODEL_NAME = 'dimsim'
